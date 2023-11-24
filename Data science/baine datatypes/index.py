5f8982ad0bfc62235af58a240746b8c0d9e33b57
nasejje mariam lumala <142495637+Bainebaiken@users.noreply.github.com>
2023-11-24 06:08:50 -0800

    datascience

---
+++ Data science/baine datatypes/index.py
@@ -0,0 +1,44 @@
+#integers(int)
+tall_men=40
+totalAmount=300
+newTotalAmount=str(totalAmount)
+total=(newTotalAmount)
+#TYPE casting in strings
+#print (type(newTotalAmount))#int to string
+#print(type(total))
+
+#strings(strng)
+currency="UGX"
+
+payment="your payment is " + currency +""+ str(newTotalAmount)#concantenartion 
+message="""
+putting  3 is called multiline commenting
+type casting is converting a variabledata type to another data type 
+and closing they must be 3
+"""
+#the output 
+print(payment)
+
+print ("your payment is"+ currency +""str(newTotalAmount))
+
+#strings as arrays
+#the first character of a string starts with zero
+name="BAINE"
+print(name[4])
+fruit="i love you "
+print(fruit[0])
+
+
+#looping through strings 
+for name in "BAINE":
+
+print(name)
+for y in fruit:
+    print(y)
+    for i in fruit:
+        print(i)
+# boolens 
+hasAttend=True
+isPowerOn=False
+print(True) 
+print(False)       
